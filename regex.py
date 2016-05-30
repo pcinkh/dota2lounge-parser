e9631106d199bdd92e657dee36927ad1d1ea91b6
pcinkh <pcinkh@gmail.com>
2016-05-30 15:00:54 +0300

    Improvments.

--- regex.py
+++ regex.py
@@ -1,32 +1,45 @@
-import urllib.request
-import re
+import re as regex
 from datetime import datetime, timedelta
 
 from pymongo import MongoClient
+from pymongo.errors import DuplicateKeyError
+import requests
 
 
+SITE_URL = 'http://dota2lounge.com/'
 
-PARSE_SITE_URL = 'http://dota2lounge.com/'
-
-HEADER = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36',
-}
 
 client = MongoClient('mongodb://localhost:27017/')
 db = client.d2l_reg
 
 
-regex = {
-    'games': r'<div class="matchmain">(.*?)</div></div></div>',
-    'live': r'(LIVE)',
-    'time': r'<div class="whenm">([0-9]+)(\s)(\w+)',
-    'match_id': r'<a href="match\?m=([0-9]+)',
-    'teams': r'<div class="team"(.*?)</div>    </div>',
-    'team_name': r'<div class="teamtext"><b>(.*?)</b>',
-    'won': r'(won.png)',
+rgx = {
+    'games': regex.compile(r'<div class="matchmain">(.*?)</div></div></div>'),
+    'live': regex.compile(r'(LIVE)'),
+    'time': regex.compile(r'<div class="whenm">([0-9]+)\s(\w+)'),
+    'match_id': regex.compile(r'<a href="match\?m=([0-9]+)'),
+    'teams': regex.compile(r'<div class="team"(.*?)</div>    </div>'),
+    'team_name': regex.compile(r'<div class="teamtext"><b>(.*?)</b>'),
+    'won': regex.compile(r'(won.png)'),
 }
 
-req = urllib.request.Request(PARSE_SITE_URL, headers=HEADER)
+
+def main():
+    html = requests.get(PARSE_SITE_URL).text.replace('\n', '').replace('\r', '')
+
+    games = split_page(
+        html,
+        '<article class="standard" id="bets" style="margin-top: 40px;">',
+        '</article>'
+    )
+
+    games_list = regex.findall(rgx['games'], games)
+
+    for game in games_list:
+        live = regex.search(rgx['live'], game)
+
+        if not live:
+            mongo_save(parse_game(game))
 
 
 def split_page(source, start, end):
@@ -49,49 +62,36 @@ def get_time(value, unit):
 def parse_game(game):
     parsed_game = {}
 
-    time_regex = re.findall(re.compile(regex['time']), game)
+    time_regex = regex.findall(rgx['time'], game)
     time_list = list(time_regex[0])
-    del time_list[1]
 
     parsed_game['time'] = get_time(time_list[0], time_list[1])
 
-    parsed_game['match_id'] = re.findall(re.compile(regex['match_id']), game)
+    parsed_game['_id'] = regex.findall(rgx['match_id'], game)[0]
 
-    team = re.findall(re.compile(regex['teams']), game)
+    team = regex.findall(rgx['teams'], game)
 
-    parsed_game['team_1'] = re.findall(re.compile(regex['team_name']), team[0])
+    parsed_game['team_1'] = regex.findall(rgx['team_name'], team[0])[0]
 
-    parsed_game['team_2'] = re.findall(re.compile(regex['team_name']), team[1])
+    parsed_game['team_2'] = regex.findall(rgx['team_name'], team[1])[0]
 
     parsed_game['won'] = 0
 
-    if re.findall(re.compile(regex['won']), team[0]):
+    if regex.findall(rgx['won'], team[0]):
         parsed_game['won'] = 1
-    elif re.findall(re.compile(regex['won']), team[1]):
+    elif regex.findall(rgx['won'], team[1]):
         parsed_game['won'] = 2
+
+    print(parsed_game)
 
     return parsed_game
 
 
 def mongo_save(game):
-    games = db.games
-    if not db.games.find_one({'match_id': game['match_id']}):
-        games.insert_one(game)
-    print(game)
+    try:
+        db.games.insert_one(game)
+    except DuplicateKeyError:
+        pass
 
-with urllib.request.urlopen(req) as response:
-    html = response.read().decode().replace('\n', '').replace('\r', '')
-
-    games = split_page(
-        html,
-        '<article class="standard" id="bets" style="margin-top: 40px;">',
-        '</article>'
-    )
-
-    games_list = re.findall(re.compile(regex['games']), games)
-
-    for game in games_list:
-        live = re.search(re.compile(regex['live']), game)
-
-        if not live:
-            mongo_save(parse_game(game))
+if __name__ == '__main__':
+    main()
